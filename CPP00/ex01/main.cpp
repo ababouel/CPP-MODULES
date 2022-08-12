150a7c8102492b2a5dbe4b2db11a4657978cf4d3
Abdelghani <abdelghani@Abdelghanis-MacBook-Air.local>
2022-08-12 20:03:57 +0100

    change some thing in main

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ababouel <ababouel@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 14:48:04 by ababouel          #+#    #+#             */
-/*   Updated: 2022/08/12 05:53:48 by ababouel         ###   ########.fr       */
+/*   Updated: 2022/08/12 06:28:46 by ababouel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,25 +25,28 @@ int main()
 	std::cout << "<<< welcome to Phonebook app >>>" << std::endl;
 	std::cout << "choose one of the option: "<< " ADD " << " SEARCH " << " EXIT " << std::endl;
 	while (1)
-	{
+	{	
+		std::cout << "> ";
 		std::getline(std::cin,write);
+		if (!std::cin.good())
+			break;
 		if (index == 8)
 			index = 0;
 		if (!write.compare("ADD"))
 		{
-			std::cout << "add first name" << std::endl;
+			std::cout << "add first name: ";
 			std::getline(std::cin, write);
 			word[0] = write;
-			std::cout << "add last name" << std::endl;
+			std::cout << "add last name : " ;
 			std::getline(std::cin, write);
 			word[1] = write;
-			std::cout << "add nick name" << std::endl;
+			std::cout << "add nick name : ";
 			std::getline(std::cin, write);
 			word[2] = write;
-			std::cout << "add phone number" << std::endl;
+			std::cout << "add phone number : ";
 			std::getline(std::cin, write);
 			word[3] = write;
-			std::cout << "add darkest number" << std::endl;
+			std::cout << "add darkest number : ";
 			std::getline(std::cin, write);
 			word[4] = write;
 			if (word[0].size() && word[1].size() && word[2].size() && word[3].size() && word[4].size())
