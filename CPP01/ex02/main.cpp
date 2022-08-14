203b7bca628921d43adb2d27366e8b874a4480e3
Abdelghani Abouelmahassine <ababouel@e1r5p10.1337.ma>
2022-08-14 21:35:44 +0100

    ex02 done

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ababouel <ababouel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 19:51:38 by ababouel          #+#    #+#             */
-/*   Updated: 2022/08/14 19:52:22 by ababouel         ###   ########.fr       */
+/*   Updated: 2022/08/14 21:33:51 by ababouel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 int main( void )
 {
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+    std::cout << &str << std::endl; 
+    std::cout << stringPTR << std::endl; 
+    std::cout << &stringREF << std::endl; 
+    std::cout << str << std::endl; 
+    std::cout << *stringPTR << std::endl; 
+    std::cout << stringREF << std::endl; 
     return (0);
 }
