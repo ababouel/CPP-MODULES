44b1e8d36c1d69b9a0baf63551e7cde98f461684
Abdelghani Abouelmahassine <ababouel@e3r1p5.1337.ma>
2022-09-07 23:27:34 +0100

    CPP02 ex00

---
+++ CPP02/ex00/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ababouel <ababouel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/07 23:19:39 by ababouel          #+#    #+#             */
+/*   Updated: 2022/09/07 23:26:43 by ababouel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.h"
+
+int main()
+{
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+}
