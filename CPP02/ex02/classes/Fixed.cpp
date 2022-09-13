54f1c958b083838bd5aa56619a23d04236c19829
Abdelghani Abouelmahassine <ababouel@e3r2p2.1337.ma>
2022-09-14 00:28:23 +0100

    update

--- CPP02/ex02/classes/Fixed.cpp
+++ CPP02/ex02/classes/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: ababouel <ababouel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 23:20:20 by ababouel          #+#    #+#             */
-/*   Updated: 2022/09/12 19:40:17 by ababouel         ###   ########.fr       */
+/*   Updated: 2022/09/14 00:27:17 by ababouel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,9 +63,112 @@ float   Fixed::toFloat(void) const
 {
     return ((float)this->pointFix / (float)(1 << this->nOFBits));
 }
+
 int     Fixed::toInt(void) const
 {
     return (this->pointFix >> this->nOFBits);
+}
+
+bool    Fixed::operator <(const Fixed &fixed)
+{
+    return (this->pointFix < fixed.pointFix ? true : false);
+}
+
+bool    Fixed::operator >(const Fixed &fixed)
+{
+    return (this->pointFix > fixed.pointFix ? true : false);
+}
+
+bool    Fixed::operator <=(const Fixed &fixed)
+{
+    return (this->pointFix <= fixed.pointFix ? true : false);
+}
+
+bool    Fixed::operator >=(const Fixed &fixed)
+{
+    return (this->pointFix >= fixed.pointFix ? true : false);
+}
+
+bool    Fixed::operator !=(const Fixed &fixed)
+{
+    return (this->pointFix != fixed.pointFix ? true : false);
+}
+
+bool    Fixed::operator ==(const Fixed &fixed)
+{
+    return (this->pointFix == fixed.pointFix ? true : false);
+}
+
+Fixed&  Fixed::operator +(const Fixed &fixed)
+{
+    this->pointFix = this->pointFix + fixed.pointFix;
+    return (*this);
+}
+
+Fixed&  Fixed::operator -(const Fixed &fixed)
+{
+    this->pointFix = this->pointFix - fixed.pointFix;
+    return (*this);
+}
+
+Fixed&  Fixed::operator *(const Fixed &fixed)
+{
+    this->pointFix = this->pointFix * fixed.pointFix;
+    return (*this);
+}
+
+Fixed&  Fixed::operator /(const Fixed &fixed)
+{
+    this->pointFix = this->pointFix / fixed.pointFix;
+    return (*this);
+}
+
+Fixed&  Fixed::operator ++()
+{
+    ++this->pointFix;
+    return (*this);
+}
+
+Fixed&  Fixed::operator ++(int)
+{
+    this->pointFix++;
+    return (*this);
+}
+
+Fixed&  Fixed::operator --()
+{
+    --this->pointFix;
+    return (*this);
+}
+
+Fixed&  Fixed::operator --(int)
+{
+    this->pointFix--;
+    return (*this);
+}
+
+Fixed& Fixed::min(Fixed& fx0, Fixed &fx1)
+{
+    return (fx0.pointFix > fx1.pointFix ? fx1 : fx0);
+}
+
+Fixed& Fixed::max(Fixed& fx0, Fixed &fx1)
+{
+    return (fx0.pointFix > fx1.pointFix ? fx0 : fx1);
+}
+
+Fixed& Fixed::min(const Fixed& fx0, const Fixed &fx1)
+{
+    Fixed fix = Fixed();
+    fix = fx0.pointFix > fx1.pointFix ? fx1.pointFix : fx0.pointFix;
+    return (fix);
+}
+
+Fixed& Fixed::max(const Fixed& fx0, const Fixed &fx1)
+{
+    Fixed fix;
+    fix = fx0.pointFix > fx1.pointFix ? fx0.pointFix : fx1.pointFix;
+    return (fix);
 }
 
 std::ostream& operator<<(std::ostream& os, const Fixed& fix)
