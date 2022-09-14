ed796737dbd03a21a387b514016c243e80a23af4
Abdelghani Abouelmahassine <ababouel@e3r4p9.1337.ma>
2022-09-14 02:16:35 +0100

    ex03 CPP01

--- CPP01/ex03/classes/HumanB.cpp
+++ CPP01/ex03/classes/HumanB.cpp
@@ -6,13 +6,13 @@
 /*   By: ababouel <ababouel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 21:45:03 by ababouel          #+#    #+#             */
-/*   Updated: 2022/09/05 21:53:13 by ababouel         ###   ########.fr       */
+/*   Updated: 2022/09/14 02:14:40 by ababouel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.h"
 
-HumanB::HumanB(std::string person) : name(person) 
+HumanB::HumanB(std::string person) : name(person), weapon(NULL) 
 {}
 
 HumanB::~HumanB()
@@ -20,7 +20,8 @@ HumanB::~HumanB()
 
 void    HumanB::attack( void ) const
 {
-    std::cout << this->name <<" attacks with their "<< this->weapon->getType() << std::endl;
+    if (this->weapon != NULL)
+        std::cout << this->name <<" attacks with their "<< this->weapon->getType() << std::endl;
 }
 
 void    HumanB::setWeapon(Weapon& weapon)
