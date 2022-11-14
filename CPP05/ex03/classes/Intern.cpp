c560e9c510777aa0851b6439b61ba9499e47fae5
Abdelghani <abdelghani@Abdelghanis-MacBook-Air.local>
2022-11-14 17:18:14 +0100

    update ex02 CPP05

--- CPP05/ex03/classes/Intern.cpp
+++ CPP05/ex03/classes/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: ababouel <ababouel@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 00:53:48 by ababouel          #+#    #+#             */
-/*   Updated: 2022/11/14 16:33:39 by ababouel         ###   ########.fr       */
+/*   Updated: 2022/11/14 17:14:24 by ababouel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ Form* Intern::makeForm(std::string name, std::string target)
 {
 	int state = 0;
 	std::string forms[3] = {"robotomy request", "shrubbery creation", "presidential pardon"};
-	void (Intern::*arForm[3])() = {&Intern::shrubbery(target), &Intern::robotomy(target), &Intern::president(target)};
+	Form* (Intern::*arForm[3])(std::string target) = {&Intern::shrubbery(target), &Intern::robotomy(target), &Intern::president(target)};
 	while (state < 3)
 	{
 		if (state < 3 && name == forms[state])
@@ -56,11 +56,11 @@ Form* Intern::makeForm(std::string name, std::string target)
 	switch (state)
 	{
 	case 0:
-		return (arForm[1]);
+		return ((this->*arForm[1])(target));
 	case 1:
-		return (arForm[0]);
+		return ((this->*arForm[0])(target));
 	case 2:
-		return (arForm[2]);
+		return ((this->*arForm[2])(target));
 	default:
 		std::cout << "this forms name :" + name + " doesn't exist!!!\n";
 		return (NULL);
