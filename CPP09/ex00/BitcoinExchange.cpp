40abf5f22defc5ceb9226f0e3788402592ce313e
Abdelghani Abouelmahassine <ababouel@e1r7p9.1337.ma>
2023-04-06 03:17:42 +0000

    update ex00

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: ababouel <ababouel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 01:28:38 by ababouel          #+#    #+#             */
-/*   Updated: 2023/04/06 02:23:48 by ababouel         ###   ########.fr       */
+/*   Updated: 2023/04/06 03:16:50 by ababouel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,32 @@
 
 BitcoinExchange::BitcoinExchange(std::string ss)
 {
-    std::ifstream infile;
-    infile.open("data.csv");
+    std::string str;
+    this->input.open(ss);
+    this->data.open("data.csv");
+    while (std::getline(std::cin, str))
+    {
+        std::strtok((char *)str.c_str(), ",");
+        str.clear();
+    }
 }
 
 BitcoinExchange::~BitcoinExchange()
-{}
+{
+    input.close();
+    data.close();
+}
+
+void   BitcoinExchange::showListDatabase()
+{
+
+}
+
+std::pair<std::string, float> splitSS(std::string str, std::string sep)
+{
+    std::stringstream    ss(str);
+    std::string          word;
+    while (!ss.eof()){
+        std::getline(ss,  word, (char *)sep.c_str());
+    }
+}
--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: ababouel <ababouel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 02:28:46 by ababouel          #+#    #+#             */
-/*   Updated: 2023/04/06 02:23:49 by ababouel         ###   ########.fr       */
+/*   Updated: 2023/04/06 03:15:43 by ababouel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,20 @@
 # include <exception>
 # include <sstream>
 # include <string>
+#include <bits/stdc++.h>
 
 class BitcoinExchange
 {
     private:
-        std::map<std::string, float>	input;
-        std::ifstream					infile;
+        std::map<std::string, float>	dataBase;
+        std::ifstream					data;
+		std::ifstream					input;
     public:
         BitcoinExchange(std::string ss);
         ~BitcoinExchange();
-        int parsingInput();
+		void	showListDatabase();
 };
+
+std::pair<std::string, float>	splitSS(std::string str, std::string sep);
 
 #endif
