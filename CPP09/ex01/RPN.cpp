1102427a232a730d1c00be36f9d27a1ca52f57e1
Abdelghani Abouelmahassine <ababouel@e1r4p5.1337.ma>
2023-04-14 02:06:06 +0000

    final push of ex01 for CPP09 just add throw exception of division by 0

--- CPP09/ex01/RPN.cpp
+++ CPP09/ex01/RPN.cpp
@@ -12,37 +12,58 @@ void    RPN::result()
     std::string var;
     
     while (std::getline(ss, var, ' ')){
-		if (!var.empty() && isNum(var))
-			st.push(std::stoi(var));
-        else if (isOP(var) && st.size() >= 2)
-            resultOP(var);
-        else
-            std::cout << "Error\n";
+        if (!var.empty())
+        {
+            if (isNum(var))
+			    st.push(std::stoi(var));
+            else if (isOP(var) && st.size() >= 2)
+                resultOP(var);
+            else
+                throw std::runtime_error("Error\n");
+        }
 	}
+    std::cout << st.top() << std::endl;
 }
 
 bool    RPN::isNum(std::string str)
 {
-
-    return (false);
+    std::string::iterator it = str.begin();
+    while (it != str.end())
+    {
+        if (!std::isdigit(*it))
+            return (false);
+        it++;
+    }
+    return (true);
 }
 
 bool			RPN::isOP(std::string str)
 {
-
+    if (str == "/" || str == "*"
+        || str == "-" || str == "+")
+        return (true);
+    else
+        return (false);
 }
 
 void    RPN::resultOP(std::string str)
 {
     op2 = st.top();
+    st.pop();
     op1 = st.top();
     st.pop();
-    st.pop();
     if (str == "/")
+    {
+        if (op2 == 0)
+            throw std::runtime_error("Error: Division by zero");
+        st.push(op1 / op2); 
+    }
     else if (str == "*")
-
+        st.push(op1 * op2); 
     else if (str == "-")
+        st.push(op1 - op2); 
     else if (str == "+")
+        st.push(op1 + op2);
     else
         std::cout << "Error\n";
 }
--- CPP09/ex01/main.cpp
+++ CPP09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ababouel <ababouel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 01:16:14 by ababouel          #+#    #+#             */
-/*   Updated: 2023/04/13 01:41:07 by ababouel         ###   ########.fr       */
+/*   Updated: 2023/04/14 01:18:10 by ababouel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,18 @@
 
 int main(int ac, char** arg)
 {
-    if (ac == 2)
+    try
     {
-        RPN rpn(arg[1]);
-        rpn.result();
+        if (ac == 2)
+        {
+            RPN rpn(arg[1]);
+            rpn.result();
+        }
+        else
+            throw std::runtime_error("please insert some op argument.\n");
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what();
     }
     return (1);
 }
